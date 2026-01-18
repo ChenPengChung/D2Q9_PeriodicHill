6d176f043eb27c98816bc057cbb11b0943598f55
s8313697 <s8313697@gmail.com>
2026-01-18 22:07:02 +0800

    auto-sync: 2026-01-18 22:07:02

--- evolution.h
+++ evolution.h
@@ -18,9 +18,10 @@ void stream_collide(
      double *f0_old, double *f1_old, double *f2_old, double *f3_old, double *f4_old, double *f5_old, double *f6_old, double *f7_old, double *f8_old,
     //f_new:本時間步所更新的物理空間計算點的碰撞後插值前一般態分佈函數
     double *f0_new, double *f1_new, double *f2_new, double *f3_new, double *f4_new, double *f5_new, double *f6_new, double *f7_new, double *f8_new, 
-    //Ｙ方向預配置連乘權重
-    double *Y0_0,  double *Y0_1, double *Y0_2,  double *Y0_3,  double *Y0_4,  double *Y0_5,  double *Y0_6,  
-    double *Y2_0,  double *Y2_1,  double *Y2_2,  double *Y2_3,  double *Y2_4,  double *Y2_5,  double *Y2_6,
+    //Ｙ方向預配置連乘權重一維連續記憶體
+    double *Y0_0,  double *Y0_1, double *Y0_2,  double *Y0_3,  double *Y0_4,  double *Y0_5,  double *Y0_6,  //(處理F1,F5,F8等等y方向插值問題)
+    double *Y2_0,  double *Y2_1,  double *Y2_2,  double *Y2_3,  double *Y2_4,  double *Y2_5,  double *Y2_6, //(處理F3,F6,F7等等y方向插值問題)
+    
     ){
 
 }

f81c8aced553f914fa3108c891f6a1a3e523f099
s8313697 <s8313697@gmail.com>
2026-01-18 21:36:44 +0800

    auto-sync: 2026-01-18 21:36:44

--- evolution.h
+++ evolution.h
@@ -6,16 +6,18 @@
 #include "MRT_Matrix.h"
 //1.
 //面法向量為ｅ_y的單位面積質量流率
-double ModifydRho_F1( double F1_in, double f2_old)
+double ModifydRho_F1( double F1_in, double f3_old)
 {
-    double drho = F1_in - f2_old;
+    double drho = F1_in - f3_old;
     return drho;
 }
 //2.
 //面法向量為－ｅ_y的單位面積質量流率
-double ModifydRho_F3( double F3_in, double f4_old)
+double ModifydRho_F3( double F3_in, double f1_old)
 {
-    double drho = F3_in - f4_old;
+    double drho = F3_in - f1_old;
     return drho;
 }
+
+
 #endif
